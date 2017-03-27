c4f265e5ad117bbb7d84d7852334e4927feb43dd
jorgevalerio <jorgevalerio921@gmail.com>
2017-03-26 17:13:41 -0700

    project11
    optimization

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp'),
+concat = require('gulp-concat'),
+uglify = require('gulp-uglify'),
+rename = require('gulp-rename');
+
+gulp.task("concatScripts", function(){
+	gulp.src([
+			'js/jquery.js',
+			'js/fastclick.js',
+			'js/foundation.js',
+			'js/foundation.equalizer.js',
+			'js/foundation.reveal.js',
+			
+		])
+	.pipe(concat("app.js"))
+	.pipe(gulp.dest('js'));
+});
+
+
+gulp.task('minifyScripts', function (){
+	gulp.src('js/app.js')
+	.pipe(uglify())
+	.pipe(rename('app.min.js'))
+	.pipe(gulp.dest('js'));
+});
+
+
+gulp.task('default', ["hello"], function(){
+		console.log("the default task");
+	});
